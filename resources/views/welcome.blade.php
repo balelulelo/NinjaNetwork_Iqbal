e83202be01851b0441b96fd45fbb4ee0b9901747
jaesekii <162032031+balelulelo@users.noreply.github.com>
2025-09-03 22:38:03 +0700

    added from tutorial 1 - 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <h1>Welcome to Ninja Network</h1>
     <p>Click the button below to view the list of ninjas.</p>
 
-    <a href="" class="btn">
+    <a href="/ninjas" class="btn">
         Find Ninjas!
     </a>
 
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ninjas', function () {
+    $ninjas = [
+        ['name' => 'Naruto Uzumaki', 'skill' => 75, "id" => 1],
+        ['name' => 'Sasuke Uchiha', 'skill' => 50, "id" => 2],
+        ['name' => 'Sakura Haruno', 'skill' => 70, "id" => 3],
+        ['name' => 'Kakashi Hatake', 'skill' => 60, "id" => 4],
+        ['name' => 'Itachi Uchiha', 'skill' => 95, "id" => 5],
+    ];
+
+    return view('ninjas.index', ["greeting" => "Hello", "ninjas" => $ninjas]);
+});
+
+Route::get('/ninjas/{id}', function ($id) {
+    $ninjas = [
+        ['name' => 'Naruto Uzumaki', 'skill' => 75, "id" => 1],
+        ['name' => 'Sasuke Uchiha', 'skill' => 50, "id" => 2],
+        ['name' => 'Sakura Haruno', 'skill' => 70, "id" => 3],
+        ['name' => 'Kakashi Hatake', 'skill' => 60, "id" => 4],
+        ['name' => 'Itachi Uchiha', 'skill' => 95, "id" => 5],
+    ];
+
+    return view('ninjas.show', ["id" => $id]);
+});
+
