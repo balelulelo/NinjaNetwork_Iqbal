94584296766bcc95749ae732efaf1980203ab6fc
jaesekii <162032031+balelulelo@users.noreply.github.com>
2025-10-01 10:39:53 +0700

    after installing breeze, my web application found several issues. i'll put this inside the breeze branch just in case, and pull again from the main branch

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NinjaController;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/ninjas', [NinjaController::class, 'index'])->name('ninjas.index');
-Route::get('/ninjas/create', [NinjaController::class, 'create'])->name('ninjas.create');
-Route::get('/ninjas/{id}', [NinjaController::class, 'show'])->name('ninjas.show');
-Route::post('/ninjas', [NinjaController::class, 'store'])->name('ninjas.store');
-Route::delete('/ninjas/{id}', [NinjaController::class, 'destroy'])->name('ninjas.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
